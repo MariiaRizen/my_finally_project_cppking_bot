791d5f51c02f2cea1f05e0b1931edd4612a7f579
MariiaRizen <mashynyar@gmail.com>
2023-01-31 09:45:51 +0200

    start project

---
+++ db/scripts.py
@@ -0,0 +1,30 @@
+import os
+import sqlite3
+from dotenv import load_dotenv
+
+load_dotenv()
+
+con = sqlite3.connect(os.environ['DB_PATH'])
+cur = con.cursor()
+
+
+def create_tables():
+    cur.execute("""CREATE TABLE cooking(
+    id PRIMARY KEY, 
+    name VARCHAR(127),
+    section VARCHAR(63),
+    recipe VARCHAR,
+    image VARCHAR(511),
+    video VARCHAR
+    
+    )""")
+
+
+def inserts_one():
+    cur.execute("""
+            INSERT INTO cooking_db VALUES()""")
+
+
+def inserts_many():
+    pass
+
